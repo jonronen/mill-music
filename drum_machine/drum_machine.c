5d707f31cb1b3e1daae82f2dc8716cc7b501e5d8
Jon Ronen-Drori <jon_ronen@yahoo.com>
2022-02-01 22:25:46 +0200

    Remove unnecessary license comment

--- drum_machine/drum_machine.c
+++ drum_machine/drum_machine.c
@@ -1,5 +1,4 @@
 /*
- *
  * Arduino-based drum machine
  * Copyright 2012-2022 Jon Ronen-Drori
  *
@@ -12,22 +11,6 @@
  * - Arduino Octosynth (http://www.instructables.com/id/The-Arduino-OctoSynth/)
  * - sfxr (http://www.drpetter.se/project_sfxr.html)
  * - Speaker PCM (http://www.arduino.cc/playground/Code/PCMAudio)
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
  */
 
 
